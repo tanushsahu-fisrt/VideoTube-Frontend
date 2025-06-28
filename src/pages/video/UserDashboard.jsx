e8842a592e826316bd633317f12775abf01da923
tanushsahu-fisrt <sahutanush29@gmail.com>
2025-06-28 09:50:15 +0530

    UserDashboard updated

---
+++ src/pages/video/UserDashboard.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+
+const UserDashboard = () => {
+
+    return(
+        <>
+        <Header/>
+        <Sidebar />
+        </>
+    )
+}
+
+export default UserDashboard;
