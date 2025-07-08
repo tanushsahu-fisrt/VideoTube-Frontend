3156dfef704c0c4d326a4f427105eb796421a021
tanushsahu-fisrt <sahutanush29@gmail.com>
2025-07-09 00:12:28 +0530

    Tweet featured Introduced

--- src/assets/logo.jsx
+++ src/assets/logo.jsx
@@ -1,13 +1,13 @@
 import { Sparkles } from "lucide-react";
 
-const VideoTubeLogoDark = () => (
+const VideoTubeLogo = () => (
   <div className="flex items-center gap-1">
     <div className="w-10 h-10 bg-gradient-to-br from-indigo-500 to-purple-600 rounded-lg flex items-center justify-center">
       <Sparkles className="w-8 h-8 text-white" />
-      <span className="text-white text-md font-bold">VT</span>
+      <span className="text-white text-md font-bold"></span>
     </div>
     <span className="font-bold text-xl text-gray-900">VideoTube</span>
   </div>
 );
 
-export default VideoTubeLogoDark;
+export default VideoTubeLogo;
